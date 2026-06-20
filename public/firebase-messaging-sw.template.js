249edea101074848842aad88541a040ba003c607
VijayveersinghCST <vijay.veer.singh@computesofttech.com>
2026-06-20 18:27:16 +0530

    uat notification

--- public/firebase-messaging-sw.template.js
+++ public/firebase-messaging-sw.template.js
@@ -2,17 +2,16 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey:            "AIzaSyDjjptCB7jYkkzbzX2dJidnBtGMByYnhlg",
-  authDomain:        "schoolspine-dev.firebaseapp.com",
-  projectId:         "schoolspine-dev",
-  storageBucket:     "schoolspine-dev.firebasestorage.app",
-  messagingSenderId: "921081546231",
-  appId:             "1:921081546231:web:4c816be1902db0f4bb1af2",
+  apiKey:            "__FIREBASE_API_KEY__",
+  authDomain:        "__FIREBASE_AUTH_DOMAIN__",
+  projectId:         "__FIREBASE_PROJECT_ID__",
+  storageBucket:     "__FIREBASE_STORAGE_BUCKET__",
+  messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+  appId:             "__FIREBASE_APP_ID__",
 });
 
 const messaging = firebase.messaging();
 
-// Background / closed tab notifications
 messaging.onBackgroundMessage((payload) => {
   const { title, body } = payload.notification || {};
   const clickAction =
@@ -22,13 +21,12 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(title || "SchoolSpine Notification", {
     body:  body || "",
-    icon:  "/logo.png",   // place your logo in /public/logo.png
-    badge: "/badge.png",  // optional 72x72 monochrome badge
+    icon:  "/logo.png",
+    badge: "/badge.png",
     data:  { clickAction },
   });
 });
 
-// Navigate to the relevant page when user clicks the notification
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   const url = event.notification.data?.clickAction || "/";
