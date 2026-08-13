f850c18269ec70b82025a24cadfd281e79ca3b1f
VijayveersinghCST <vijay.veer.singh@computesofttech.com>
2026-08-13 11:29:55 +0530

    template for salary slip

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -13,6 +13,7 @@ import { useState, useEffect, useContext, useRef, useMemo } from 'react'
 import { UserContext } from '../ContextAPI/UserContext'
 import { PERMISSIONS as P, SYSTEM_ROLES } from '../Constants/Permission'
 import OverdueFeeNotifications from "../Pages/FeeManagement/Overduefeenotification.jsx";
+import SalarySlip from "../Pages/Payroll/Salaryslipgenerator.jsx";
 
 const SCHOOL_SWITCHER_ROLES = SYSTEM_ROLES.SCHOOL_SWITCHER;
 
@@ -209,6 +210,7 @@ const menuSections = [
                     { label: 'Certificate Templates', route: '/templates/certificate', icon: Award },
                     { label: 'Fee Receipt Templates', route: '/templates/feeReceipt', icon: Receipt },
                     { label: 'Gate Pass Templates', route: '/templates/gatePass', icon: DoorOpen },
+                    { label: 'Salary Slip Templates', route: '/templates/salarySlip', icon: Wallet },
                     // { label: 'Visitor Pass Templates', route: '/templates/visitorPass', icon: UserCheck },
                     // { label: 'Cycle Stand Templates', route: '/templates/cycleStandPass', icon: Bike },
                 ]
---
+++ src/Pages/Templates/SalarySlipTemplates.jsx
@@ -0,0 +1,5 @@
+import PrintTemplatesPage from '../../Components/Templates/PrintTemplatesPage'
+
+export default function SalarySlipTemplates() {
+    return <PrintTemplatesPage type="SALARY_SLIP" />
+}
